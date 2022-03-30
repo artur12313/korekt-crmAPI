e223b0e0a6e94e3b33482a6f2488ea89591f5466
artur12313 <arturpisula11@wp.pl>
2022-03-30 12:14:01 +0200

    Added RegisterRoute to Router

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 //public routes
+Route::post('/register', [AuthController::class, 'register']);
 Route::get('/products', function() {
     return 'products';
 });
