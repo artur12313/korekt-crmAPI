e9ae632fabc8ef848ddd291ed49788238f60e34f
artur12313 <arturpisula11@wp.pl>
2022-03-30 12:33:47 +0200

    Added function login

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\AuthController;
 
 //public routes
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::get('/products', function() {
     return 'products';
 });
