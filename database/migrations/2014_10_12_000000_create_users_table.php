4b56c8ab3d81344fda03ce5b6c5a41be2905da54
Artur Pisula <ap057197@student.ath.edu.pl>
2022-03-31 19:54:20 +0200

    Added phone nullable column in DB

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone', 12);
-            $table->string('image');
+            $table->string('phone', 12)->nullable();
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
