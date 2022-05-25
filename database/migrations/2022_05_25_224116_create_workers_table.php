986605dfa5ed1d9c1c442d3ba15a2c2c67ba443f
Artur Pisula <arturpisula11@wp.pl>
2022-05-26 00:45:56 +0200

    Added Workers Table

---
+++ database/migrations/2022_05_25_224116_create_workers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->id();
+            $table->string('First_name');
+            $table->string('Last_name');
+            $table->string('phone');
+            $table->string('Bank_account_number');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workers');
+    }
+};
