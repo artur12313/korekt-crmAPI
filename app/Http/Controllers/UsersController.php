4d933461f77a5aac277019d0f39bf749d616eab0
artur12313 <arturpisula11@wp.pl>
2022-09-12 23:43:26 +0200

    Added roles and permissions

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class UsersController extends Controller
+{
+    /**
+     * Display all users
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function index() 
+    {
+        $users = User::all();
+        $roles = Role::all();
+
+        foreach($users as $user)
+        {
+            $item [] = ['id' => $user->id, 'name' => $user->name, 'email' => $user->email, 'phone' => $user->phone, 'role' => $user->role, 'created_at' => date('d-m-Y', strtotime($user->created_at))];
+        }
+
+        // return view('users.index', compact('users'));
+        return response()->json(['status' => 200, 'users' => $item]);
+    }
+
+    /**
+     * Show form for creating user
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function create() 
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created user
+     * 
+     * @param User $user
+     * @param StoreUserRequest $request
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function store(User $user, StoreUserRequest $request) 
+    {
+        //For demo purposes only. When creating user or inviting a user
+        // you should create a generated random password and email it to the user
+        $user->create(array_merge($request->validated(), [
+            'password' => 'test' 
+        ]));
+
+        return redirect()->route('users.index')
+            ->withSuccess(__('User created successfully.'));
+    }
+
+    /**
+     * Show user data
+     * 
+     * @param User $user
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user) 
+    {
+        return view('users.show', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Edit user data
+     * 
+     * @param User $user
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user) 
+    {
+        return view('users.edit', [
+            'user' => $user,
+            'userRole' => $user->roles->pluck('name')->toArray(),
+            'roles' => Role::latest()->get()
+        ]);
+    }
+
+    /**
+     * Update user data
+     * 
+     * @param User $user
+     * @param UpdateUserRequest $request
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function update(User $user, UpdateUserRequest $request) 
+    {
+        $user->update($request->validated());
+
+        $user->syncRoles($request->get('role'));
+
+        return redirect()->route('users.index')
+            ->withSuccess(__('User updated successfully.'));
+    }
+
+    /**
+     * Delete user data
+     * 
+     * @param User $user
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user) 
+    {
+        $user->delete();
+
+        return redirect()->route('users.index')
+            ->withSuccess(__('User deleted successfully.'));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\RolesController;
+use App\Http\Controllers\PermissionsController;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,10 +35,25 @@ Route::get('/products', function() {
 Route::post('/passwordUpdate', [AuthController::class, 'updatePassword']);
 Route::post('/profileUpdate', [AuthController::class, 'profileUpdate']);
 Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/users', [UsersController::class, 'index']);
 Route::post('/category/new', [CategoryController::class, 'store']);
 Route::group(['middleware' => ['auth:passport']], function() {
     Route::get('user/{email}', [AuthController::class, 'userDetail']);
     // Route::get('/products', [ProductsController::class, 'index']);
     Route::post('/logout', [AuthController::class, 'logout']);
     
+
+
+// Route::group(['prefix' => 'users'], function() {
+//     Route::get('/users', 'UsersController@index')->name('users.index');
+//     Route::get('/create', 'UsersController@create')->name('users.create');
+//     Route::post('/create', 'UsersController@store')->name('users.store');
+//     Route::get('/{user}/show', 'UsersController@show')->name('users.show');
+//     Route::get('/{user}/edit', 'UsersController@edit')->name('users.edit');
+//     Route::patch('/{user}/update', 'UsersController@update')->name('users.update');
+//     Route::delete('/{user}/delete', 'UsersController@destroy')->name('users.destroy');
+//     });
+
+Route::resource('roles', RolesController::class);
+Route::resource('permissions', PermissionsController::class);
 });
